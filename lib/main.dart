0d4cd411fb7b911b9b2936e8622ff426b8d8f2cb
HelmoFilho <helmofilho09@gmail.com>
2021-08-06 23:15:19 -0300

    hello runApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
 
+// main function of the app
+void main() {
+  runApp(MyApp());
+}
+
+// it´s necessary to create a class to create a object in dart
+// extends means inheritance here so MyApp will inherit from someone
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text("Hello!"),
+    );
+  }
+}
